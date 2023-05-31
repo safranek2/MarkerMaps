4dca14eb492f91f366568fc190931c3f9b16ae41
Jakub Svoboda <118892397+FortunaHapo@users.noreply.github.com>
2023-05-31 09:57:46 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,26 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var map; 
+document.addEventListener("DOMContentLoaded", function () {
+  var map;
   var markers = [];
 
- 
+
   var loginForm = document.querySelector("form");
 
-  
-  loginForm.addEventListener("submit", function(event) {
-    event.preventDefault(); // Zabrání přirozenému chování odeslání formuláře
 
-    
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+
     var usernameInput = loginForm.querySelector("input[name='rusername']");
     var passwordInput = loginForm.querySelector("input[name='rpassword']");
     var username = usernameInput.value;
     var password = passwordInput.value;
 
-   
-    setTimeout(function() {
-      if (username === "spravne-uzivatelske-jmeno" && password === "spravne-heslo") {
+
+    setTimeout(function () {
+      if (username === "správné-uživatelské-jméno" && password === "správné-heslo") {
         alert("Přihlášení úspěšné!");
 
-      
+
         initializeMap();
         enableMarkerCreation();
         enableMarkerEditing();
@@ -29,36 +29,36 @@ document.addEventListener("DOMContentLoaded", function() {
       }
       usernameInput.value = "";
       passwordInput.value = "";
-    }, 1000); 
+    }, 1000);
   });
 
-  
+
   function initializeMap() {
-    
+
     map = L.map('map').setView([51.505, -0.09], 13);
 
-    
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
       maxZoom: 18,
     }).addTo(map);
 
-  
+
     var savedMarkers = loadMarkersFromDatabase();
     if (savedMarkers) {
-      savedMarkers.forEach(function(markerData) {
+      savedMarkers.forEach(function (markerData) {
         addMarker(markerData);
       });
     }
   }
 
   function enableMarkerCreation() {
-   
-    map.on('click', function(event) {
+
+    map.on('click', function (event) {
       var latitude = event.latlng.lat;
       var longitude = event.latlng.lng;
 
-      
+
       var markerData = {
         lat: latitude,
         lng: longitude,
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
- 
+
   function addMarker(markerData) {
     var marker = L.marker([markerData.lat, markerData.lng]).addTo(map);
     marker.bindPopup(markerData.name).openPopup();
@@ -80,34 +80,34 @@ document.addEventListener("DOMContentLoaded", function() {
     markers.push(marker);
   }
 
-  // Uložení značky do databáze
+
   function saveMarkerToDatabase(markerData) {
-    // Tady bysme měli provést nějaký "volání na backend server", kterej má uložit marker do databáze
-    
+    // Tady bysme měli provést nějaký "volání na backend server", který má uložit marker do databáze
+
   }
 
   // Načtení uložených značek z databáze
   function loadMarkersFromDatabase() {
     // Tady bysme měli provést "volání na backend server", který načte markery z databáze
-    
+
     return null;
   }
 
- 
+
   function deleteMarkers() {
-    markers.forEach(function(marker) {
+    markers.forEach(function (marker) {
       map.removeLayer(marker);
     });
     markers = [];
 
     // Tady bysme měli provést "volání na backend server", který smaže markery z databáze
-    
+
   }
 
-  
+
   function enableMarkerEditing() {
-    markers.forEach(function(marker) {
-      marker.on('click', function(event) {
+    markers.forEach(function (marker) {
+      marker.on('click', function (event) {
         var newMarkerName = prompt("Zadejte nový název místa:");
         if (newMarkerName) {
           marker.setPopupContent(newMarkerName);
@@ -122,9 +122,9 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
-  
+
   function updateMarkerInDatabase(markerData) {
     // Tady bysme měli provést "volání na backend server", který aktualizuje marker v databázi
-    
+
   }
 });
