b90f7e8205c11349b2bead0e93ccd1557f710650
safranek2 <safranek2@spsejecna.cz>
2023-06-04 18:08:56 +0200

    Some Fixes
    Co-Authored-By: semba77 <86146568+semba77@users.noreply.github.com>

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "";
-    $db_name ="map_user_database";
+    $db_name ="MarkerMaps";
     $conn = "";
 
     try{
---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE MarkerMaps
+USE MarkerMaps
+
+create table _user (
+    id_use INT AUTO_INCREMENT PRIMARY KEY,
+    uz_jmen varchar(15),
+    heslo char(200)
+);
+
+CREATE TABLE marker (
+    id_mar INT AUTO_INCREMENT PRIMARY KEY,
+    latitude  DECIMAL(17, 15),
+    longitude DECIMAL(17, 15),
+    name VARCHAR(40),
+    id_use INT
+);
